0ad07d97b039d30555c9f4acf9a7c090168d3b7d
bashirnayal <bashirnayal.99@gmail.com>
2022-04-29 15:31:44 +0200

    removed duplicate code

--- lexer.cc
+++ lexer.cc
@@ -1,87 +1,6 @@
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
-#include <map>
-#include <memory>
-#include <string>
-#include <utility>
-#include <vector>
-#include <iostream>
-#include "unistd.h"
-#include "string.h"
-#include "vector"
+#include "lexer.h"
 
-#define MAX_MAPPING_LEN 64
-enum Token {
-    EOF_t,
-
-    EXTERN_t, RETURN_t,
-    IF_t, ELSE_t, WHILE_t, DO_t,
-    FOR_t, BREAK_t, CONTINUE_t,
-    
-
-    ID_t, SEMICOL_t, TYPE_t,
-    
-    INTCONST_t, FLOATCONST_t,
-    DOUBLECONST_t, BOOLCONST_t,
-    CHARCONST_t,
-
-
-    ASSIGN_t, EQ_t, NE_t,
-    LT_t, GT_t, LE_t, GE_t,
-    PLUS_t, MIN_t, DIV_t, 
-    MUL_t, MOD_t, AND_t, OR_t,
-    XOR_t, NOT_t,
-
-
-
-    LPAREN_t, RPAREN_t,
-    LBRACK_t, RBRACK_t,
-    LBRACE_t, RBRACE_t,
-    COMMA_t,
-
-    UNKNOWN_t
-
-};
-enum Type {
-  INT_t,
-  FLOAT_t,
-  DOUBLE_t,
-  BOOL_t,
-  CHAR_t
-  
-};
-char reserved_words_mapping[MAX_MAPPING_LEN][MAX_MAPPING_LEN] = {
-  ";"     , "="     , "==", "!="  ,
-  "<"     , ">"     , "<=", ">="  ,
-  "+"     , "-"     , "/" , "*"   , 
-  "%"     , "&"     , "|" , "^"   , 
-  "!"     , "("     , ")" , "["   , 
-  "]"     , "{"     , "}" , ","    
-};
-Token tokens_mapping[MAX_MAPPING_LEN] = {
-  SEMICOL_t , ASSIGN_t, EQ_t    , NE_t    ,
-  LT_t      , GT_t    , LE_t    , GE_t    ,
-  PLUS_t    , MIN_t   , DIV_t   , MUL_t   ,
-  MOD_t     , AND_t   , OR_t    , XOR_t   ,
-  NOT_t     , LPAREN_t, RPAREN_t, LBRACK_t,
-  RBRACK_t  , LBRACE_t, RBRACE_t, COMMA_t
-};
-
-typedef struct token_s {
-  Token type;
-  union {
-    double double_value;
-    int32_t int_value;
-    bool bool_value;
-    uint32_t id;
-    Type data_type;
-    char * name;
-  }data;
-} token_st;
-
-
-static token_st gettok() {
+token_st gettok(void) {
   std::string read_string;
   token_st token = {};
   static int last_char = ' ';
@@ -339,19 +258,3 @@ void print_token(token_st token) {
 }
 
 
-int main() {
-
-
-  // Prime the first token.
-    int i = 0;
-    token_st token = {};
-    token.type = UNKNOWN_t;
-    while(true) {
-      token_st token = gettok();
-      print_token(token);
-      // free(token.data.name);
-      if (token.type == EOF_t) break;
-    }
-
-  return 0;
-}
